264c0548f0093ed11a237ab48541a84c1c946111
karuan <kruan@andrew.cmu.edu>
2016-12-12 01:04:31 -0500

    Started graphical display of monopoly board

--- monopoly.js
+++ monopoly.js
@@ -1,4 +1,8 @@
-function main(){
+
+var canvas = null;
+var ctx = null;
+
+function init(){
 
    console.log("Charizard says hello world!");
    var game = new gameBoard();
@@ -6,11 +10,19 @@ function main(){
    for (i=0;i<40; i++){
       console.log(game[i].name);
 
-   }
-}
+   };
+   canvas=document.getElementById("myCanvas");
+   canvas.width=750;
+   canvas.height=750;
+   ctx=canvas.getContext("2d");
+   var img = new Image();
+   img.src="board.jpeg";
+   ctx.drawImage(img,0,0,750, 750);
 
-main();
 
+};
+
+window.onload=init;
 
 
 
